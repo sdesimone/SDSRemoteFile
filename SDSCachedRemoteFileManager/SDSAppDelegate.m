51347e64a442029e23dcf0b55bfa1b597eafaa27
Sergio De Simone <sdesimone@gmx.net>
2013-06-07 12:39:46 +0200

    initial commit of cleaned-up code

--- SDSCachedRemoteFileManager/SDSAppDelegate.m
+++ SDSCachedRemoteFileManager/SDSAppDelegate.m
@@ -10,15 +10,9 @@
 
 @implementation SDSAppDelegate
 
-- (void)dealloc
-{
-    [_window release];
-    [super dealloc];
-}
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
